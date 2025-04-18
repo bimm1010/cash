f921b6c858c49406bf50e3ac313b12f4e1395941
Hoàng Nam <hoangnam@Hoangs-Mac-mini.local>
2025-04-18 11:05:15 +0700

    add .gitignore

---
+++ CaseManagerTests/CaseManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaseManagerTests.swift
+//  CaseManagerTests
+//
+//  Created by Hoàng Nam on 8/4/25.
+//
+
+import Testing
+@testable import CaseManager
+
+struct CaseManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
